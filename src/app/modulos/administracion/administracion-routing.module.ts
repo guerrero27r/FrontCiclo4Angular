ad75ab4fe70ab481546d297bbf0122d225ba98d1
Luis Guerrero <luisenriqueguerrero77@gmail.com>
2022-11-13 20:51:48 -0500

    enrutamiento

--- src/app/modulos/administracion/administracion-routing.module.ts
+++ src/app/modulos/administracion/administracion-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuscarUsuarioComponent } from './usuarios/buscar-usuario/buscar-usuario.component';
+import { CrearUsuarioComponent } from './usuarios/crear-usuario/crear-usuario.component';
+import { EditarUsuarioComponent } from './usuarios/editar-usuario/editar-usuario.component';
+import { EliminarUsuarioComponent } from './usuarios/eliminar-usuario/eliminar-usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'crear-usuario',
+    component: CrearUsuarioComponent,
+  },
+  {
+    path: 'buscar-usuario',
+    component: BuscarUsuarioComponent,
+  },
+  {
+    path: 'editar-usuario',
+    component: EditarUsuarioComponent,
+  },
+  {
+    path: 'eliminar-usuario',
+    component: EliminarUsuarioComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdministracionRoutingModule { }
+export class AdministracionRoutingModule {}
